efd08da9fd99b9f22d0eee8c91df154eaf06ef48
Justin Holmes <justinmichaelholmes@gmail.com>
2016-08-11 14:49:48 -0600

    supporting legacy docker image & tag syntax

--- src/com/rhc/DockerClient.groovy
+++ src/com/rhc/DockerClient.groovy
@@ -23,17 +23,58 @@ def CommandOutput push( String repositoryString ){
 }
 
 def CommandOutput tagImage( String imageId, String repositoryStringWithVersion ){
+	def String dockerVersion = getDockerClientVersionTrimmedString()
+	if (imageId == null || imageId.empty ){
+		println( 'imageId is empty' )
+	}
+	return tagImage( imageId, repositoryStringWithVersion, dockerVersion )
+}
+
+def CommandOutput tagImage( String imageId, String repositoryStringWithVersion, String dockerVersion ){
+	def oneNinePattern = getDockerOneNineRegex()
+	def command
+	if ( dockerVersion =~ oneNinePattern ){
+		command = "docker tag -f ${imageId} ${repositoryStringWithVersion}" // this is not unit tested currently
+	} else {
+		command = "docker tag ${imageId} ${repositoryStringWithVersion}"
+	}
 	def commandExecutor = new CommandExecutor();
-	def command = "docker tag ${imageId} ${repositoryStringWithVersion}"
 	def CommandOutput output = commandExecutor.executeInJenkinsOrGroovy( command )
 	return output
 }
 
-def CommandOutput retrieveImageId( String repositoryStringWithVersion ){
+def CommandOutput retrieveImageId( String repositoryStringWithVersion, String dockerVersion ){
+	def oneNinePattern = getDockerOneNineRegex()
+	def command
+	def legacyAPI = false
+	if ( dockerVersion =~ oneNinePattern ){
+		command = "docker images ${repositoryStringWithVersion}"
+		legacyAPI = true
+	} else {
+		command = "docker images --format '{{.ID}}' ${repositoryStringWithVersion}"
+	}
 	def commandExecutor = new CommandExecutor();
-	def command = "docker images --format '{{.ID}}' ${repositoryStringWithVersion}"
 	def CommandOutput output = commandExecutor.executeInJenkinsOrGroovy( command )
+	if ( dockerVersion =~ oneNinePattern ){
+		def outputString = output.standardOut
+		def outputTokens = outputString.split( '\\s+' )
+		def boolean found = false
+		for ( int i=0; i<outputTokens.length; i++ ){
+			if ( ( outputTokens[i] =~ getDockerOneTenImageIdRegex() ) || outputTokens[i] =~ getDockerOneNineImageIdRegex() ) {
+				output.standardOut = outputTokens[i]
+				found = true
+			}
+		}
+		if ( found == false ){
+			output.standardOut = ''
+		}
+	}
 	return output
+}
+
+def CommandOutput retrieveImageId( String repositoryStringWithVersion ){
+	def String dockerVersion = getDockerClientVersionTrimmedString()
+	return retrieveImageId( repositoryStringWithVersion, dockerVersion )
 }
 
 def CommandOutput promoteImageBetweenRepositories( String currentRepositoryWithVersion, String newRepositoryWithVersion ){
@@ -55,6 +96,23 @@ def String trimImageId( String imageIdString ){
 	}
 }
 
+def CommandOutput getDockerClientVersion(){
+	def commandExecutor = new CommandExecutor();
+	def command = "docker -v"
+	def CommandOutput output = commandExecutor.executeInJenkinsOrGroovy( command )
+	return output
+}
+
+def String getDockerClientVersionTrimmedString(){
+	def CommandOutput output = getDockerClientVersion()
+
+	def tokens = output.standardOut.split(' ')
+	def versionToken = tokens[2]
+	def versionString = versionToken.substring(0, versionToken.size()-1)
+
+	return versionString
+}
+
 def String buildRepositoryString( String host, String projectName, String imageName ){
 	return "${host}/${projectName}/${imageName}"
 }
@@ -62,4 +120,23 @@ def String buildRepositoryString( String host, String projectName, String imageN
 def String buildRepositoryStringWithVersion( String host, String projectName, String imageName, String version ){
 	def repositoryString = buildRepositoryString( host, projectName, imageName )
 	return "${repositoryString}:${version}"
+}
+
+def getDockerOneTenImageIdRegex(){
+	return ~/[0-9a-f]{12}/
+}
+
+def getDockerOneNineImageIdRegex(){
+	return ~/sha256:[0-9a-f]{5}/
+}
+
+def getImageIdRegex(){
+	if ( getDockerClientVersionTrimmedString() =~ getDockerOneNineRegex() ){
+		return getDockerOneNineImageIdRegex()
+	} else {
+		return getDockerOneTenImageIdRegex()
+	}
+}
+def getDockerOneNineRegex(){
+	return ~/1.9.[0-9]+/
 }
--- src/test/groovy/com/rhc/DockerClientTests.groovy
+++ src/test/groovy/com/rhc/DockerClientTests.groovy
@@ -4,7 +4,7 @@ import org.junit.BeforeClass;
 import org.junit.Test
 
 class DockerClientTests {
-	
+
 	def static dockerRegistryHost = 'registry.env3-1.innovation.labs.redhat.com'
 	def static openShiftHost = 'env3-1-master.innovation.labs.redhat.com'
 	def static projectName = 'holmes-stage'
@@ -13,65 +13,82 @@ class DockerClientTests {
 	def static dockerClient = new DockerClient()
 	def static openShiftClient = new OpenShiftClient()
 
-	
+
 	@BeforeClass
 	static void shouldLoginToRegistry(){
 		openShiftClient.login( openShiftHost )
 		def CommandOutput output = dockerClient.login( dockerRegistryHost, openShiftClient.getTrimmedUserToken() )
 		assert output.standardOut.contains( 'Login Succeeded' )
 	}
-	
+
 	@Test
 	void shouldFailToPullImage(){
 		def CommandOutput output = dockerClient.pull( getRepositoryBadString() )
 		assert output.standardErr.contains( 'not found' )
 	}
-	
+
 	@Test
 	void shouldSuccessfullyPullImage(){
 		def CommandOutput output = dockerClient.pull( getRepositoryGoodString() )
 		assert output.standardOut.toLowerCase().contains( 'digest' )
 	}
-	
+
 	@Test
 	void shouldFailToPushImage(){
 		def CommandOutput output = dockerClient.push( getRepositoryBadString() )
 		assert output.standardErr.contains( 'Repository does not exist' )
 	}
-	
+
 	@Test
 	void shouldSuccessfullyPushImage(){
 		def CommandOutput output = dockerClient.push( getRepositoryGoodString( ))
 		assert output.standardOut.toLowerCase().contains( 'digest' )
 	}
-	
+
 	@Test
 	void shouldFailToTagImage(){
 		def CommandOutput output = dockerClient.tagImage( '1234', getRepositoryBadStringWithVersion() )
 		assert output.standardErr.toLowerCase().contains( 'no such id: 1234' )
 	}
-	
+
 	@Test
 	void shouldSuccessfullyTagImage(){
-		def CommandOutput output = dockerClient.tagImage( 'cca3762c12a3', getRepositoryGoodStringWithVersion() )
+		def CommandOutput imageId = dockerClient.retrieveImageId( getRepositoryGoodStringWithVersion( ) )
+		def CommandOutput output = dockerClient.tagImage( dockerClient.trimImageId( imageId.standardOut ), getRepositoryGoodStringWithVersion() )
 		assert output.standardErr.empty
 		assert output.standardOut.empty
 	}
-	
+
 	@Test
 	void shouldFailToRetrieveImageId(){
 		def CommandOutput output = dockerClient.retrieveImageId( getRepositoryBadStringWithVersion() )
 		assert output.standardErr.empty
 		assert output.standardOut.empty
 	}
-	
+
 	@Test
 	void shouldSucessfullyRetrieveImageId(){
 		def CommandOutput output = dockerClient.retrieveImageId( getRepositoryGoodStringWithVersion() )
+		def pattern = dockerClient.getImageIdRegex()
 		assert output.standardErr.empty
-		assert !output.standardOut.empty
+		assert dockerClient.trimImageId( output.standardOut ) ==~ pattern
 	}
-	
+
+	@Test
+	void shouldSucessfullyRetrieveImageIdWithLegacyFormat(){
+		def CommandOutput output = dockerClient.retrieveImageId( getRepositoryGoodStringWithVersion(), '1.9.0' )
+		def pattern = dockerClient.getImageIdRegex()
+		assert output.standardErr.empty
+		assert dockerClient.trimImageId( output.standardOut ) ==~  pattern
+	}
+
+	@Test
+	void shouldFailToRetrieveImageIdWithLegacyFormat(){
+		def CommandOutput output = dockerClient.retrieveImageId( getRepositoryBadStringWithVersion(), '1.9.0' )
+		assert output.standardErr.empty
+		assert output.standardOut.empty
+	}
+
 	@Test
 	void shouldSuccessfullyPromoteImageBetweenRepositories(){
 		def currentRepoString = getRepositoryGoodStringWithVersion()
@@ -79,27 +96,42 @@ class DockerClientTests {
 		def CommandOutput output = dockerClient.promoteImageBetweenRepositories( currentRepoString, newRepoString )
 		assert output.standardOut.toLowerCase().contains( 'digest' )
 	}
-	
+
 	@Test
 	void shouldTrimImageString(){
-		def CommandOutput imageId = dockerClient.retrieveImageId( getRepositoryGoodStringWithVersion() ) 
+		def CommandOutput imageId = dockerClient.retrieveImageId( getRepositoryGoodStringWithVersion() )
 		def String trimmedImageIdString = dockerClient.trimImageId( imageId.standardOut )
 		def pattern = ~/[0-9a-f]+/
 		assert trimmedImageIdString =~ pattern
 	}
-	
+
+	@Test
+	void shouldGetDockerVersion(){
+		CommandOutput output = dockerClient.getDockerClientVersion()
+		assert output.standardErr.isEmpty()
+		assert !output.standardOut.isEmpty()
+		assert output.standardOut.toLowerCase().contains( 'version' )
+	}
+
+	@Test
+	void shouldGetDockerVersionTrimmedString(){
+		String versionString = dockerClient.getDockerClientVersionTrimmedString()
+		def pattern = ~/[0-9]+.[0-9]+.[0-9]+/
+		assert versionString =~ pattern
+	}
+
 	String getRepositoryGoodString(){
 		return dockerClient.buildRepositoryString( dockerRegistryHost, projectName, imageName )
 	}
-	
+
 	String getRepositoryGoodStringWithVersion(){
 		return dockerClient.buildRepositoryStringWithVersion( dockerRegistryHost, projectName, imageName, 'latest' )
 	}
-	
+
 	String getRepositoryBadString(){
 		return dockerClient.buildRepositoryString( dockerRegistryHost, projectName, 'foobar' )
 	}
-	
+
 	String getRepositoryBadStringWithVersion(){
 		return dockerClient.buildRepositoryStringWithVersion( dockerRegistryHost, projectName, 'foobar', 'latest' )
 	}
